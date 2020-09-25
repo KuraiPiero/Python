1299efb4f429d88b049dd08ee28db2b665c53475
KuraiPiero <https://github.com/KuraiPiero>
2020-09-24 19:43:39 -0500

    Revision

--- Listas/anexarLSegunPar.py
+++ Listas/anexarLSegunPar.py
@@ -1,19 +1,21 @@
-## python analiza a i dividido 2 y su residuo, si este da 0 entonces procede, la logica detras de esto es
-# usar el metodo de comprobar si es par o no para saltarse 1 elemento, y adicionalmente element puede
-# tener nombre el objetivo aqui es tomar ese elemento y luego enexarlo con c que tiene el parametro i que
-# i indica la posicion del elemento
-def skip_elements(letras):
+def skip_elements(elements):
+    # Initialize variables
     new_list = []
-    posicionElemento = 0
-    for letra in letras:
-        if posicionElemento % 2 == 0:
-            letraAnexar = letras[posicionElemento]
-            new_list.append(letraAnexar)
-        posicionElemento += 1
+    i = 0
+    # Iterate through the list
+    for element in elements:
+        # Does this element belong in the resulting list?
+        if i % 2 == 0:
+            new_list.append(elements[i])
+            # Add this element to the resulting list
+        # Increment i
+        i += 1
     return new_list
 
 
-print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))
-print(skip_elements(["Orange", "Pineapple", "Strawberry", "Kiwi", "Peach"]))
-print(skip_elements([]))
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))  # Should be ['a', 'c', 'e', 'g']
+print(
+    skip_elements(["Orange", "Pineapple", "Strawberry", "Kiwi", "Peach"])
+)  # Should be ['Orange', 'Strawberry', 'Peach']
+print(skip_elements([]))  # Should be []
 
