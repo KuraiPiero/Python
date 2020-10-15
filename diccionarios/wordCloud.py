a7468afc554353fbbe9c2549acc86a17dd9408f4
KuraiPiero <https://github.com/KuraiPiero>
2020-10-15 18:44:31 -0500

    nube de letras funcional

--- diccionarios/wordCloud.py
+++ diccionarios/wordCloud.py
@@ -1,11 +1,61 @@
-import wordcloud
+from wordcloud import WordCloud, STOPWORDS
 import wikipedia
+import os
+from PIL import Image
+import numpy as np
+
+stop_words = [
+    "el",
+    "la",
+    "un",
+    "una",
+    "ella",
+    "ellos",
+    "en",
+    "unos",
+    "yo",
+    "tu",
+    "nosotros",
+    "vosotros",
+    "con",
+    "del",
+    "es",
+    "la",
+    "lo",
+    "por",
+    "los",
+    "se",
+    "las",
+    "mas",
+    "más",
+    "al",
+    "que",
+    "de",
+    "ISBN",
+    "sino" "para",
+    "otro",
+    "esta",
+    "entre",
+    "otro",
+    "su",
+] + list(STOPWORDS)
 
 
-def get_words(query):
+cur_dir = os.path.dirname(__file__)
+
+
+def get_wiki(query):
+    wikipedia.set_lang("es")
     title = wikipedia.search(query)[0]
     page = wikipedia.page(title)
     return page.content
 
 
-print(get_words("Belleza"))
+def create_word_cloud_img(text):
+    mask = np.array(Image.open(os.path.join(cur_dir, "mask.png")))
+    wc = WordCloud(background_color="white", mask=mask, max_words=200, stopwords=stop_words)
+    wc.generate(text)
+    wc.to_file(os.path.join(cur_dir, "image.png"))
+
+
+create_word_cloud_img(get_wiki("Poesia"))
